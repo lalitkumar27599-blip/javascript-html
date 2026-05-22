8b6c8d54d4b8e4b6ca7bd4245e151112626c923a
LALIT KUMAR <lalitkumar27599@gmail.com>
2026-05-22 15:43:27 +0530

    index file update

---
+++ index.js
@@ -0,0 +1,37 @@
+var studentName = "Lalit";
+console.log(studentName);
+
+let age = 20;
+age = 26;
+console.log(age);
+
+const country = "India";
+console.log(country);
+
+let name = "Lalit";
+let age2 = 23;
+let city = "Jalore";
+console.log(name, age2, city);
+
+let a=5;
+let b=7;
+let c=a+b;
+console.log(c);
+
+let str1 = "JavaScript";
+console.log(str1[0]);
+
+let str2 = "Programming";
+console.log(str2[10]);
+
+let str3 = "HELLO WORLD";
+console.log(str3.toLowerCase());
+
+let str4 = "javascript";
+console.log(str4.toUpperCase());
+
+let student = {
+        name: "lalit",
+        course: "pca",
+    }
+console.log(student);
